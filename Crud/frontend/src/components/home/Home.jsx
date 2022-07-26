8aa9909990ff91a248c710895ab5c08383901943
Jamess Miranda <jamerson193@gmail.com>
2022-07-25 23:20:41 -0300

    Cadastro de Usuario

---
+++ Crud/frontend/src/components/home/Home.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+import Main from "../../template/Main";
+
+export default props =>
+    <Main icon="home" title="inicio" subtitle="Projeto de Cadastro">
+        <div className='display-4'>Bem Vindo!</div>
+        <hr />
+        <p className="mb-0">Esse Sistema de Cadastro de Usuario foi criado no intuito
+        de ter como um portifolio totalmente Desenvolvido em React</p>
+    </Main>
---
+++ Crud/frontend/src/template/Footer.jsx
@@ -0,0 +1,10 @@
+import './Footer.css'
+import React from 'react'
+
+export default props =>
+    <footer className="footer">
+        <span>
+        Criado por <i className="fa fa-heart text-danger"></i> Dev
+            <strong> James <span className="text-danger">Miranda</span></strong>
+        </span>
+    </footer>
